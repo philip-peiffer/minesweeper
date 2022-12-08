bc4d474d7f5de28b86febebd652264f8b372011e
Philip Peiffer <philip.peiffer@gmail.com>
2022-12-07 17:21:51 -0700

    starting over on mine module

--- mine.py
+++ mine.py
@@ -1,10 +1,7 @@
 # This module contains an abstraction for the mines used in the game
+from space import Space
 
-class Mine:
+class Mine(Space):
 
-    def __init__(self, x_pos: int, y_pos: int) -> None:
-        self.position = (x_pos, y_pos)
-        self.neighbors = self.find_neighbors()
-
-    def find_neighbors(self) -> list:
-        return []
+    def __init__(self) -> None:
+        pass
