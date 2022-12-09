aafa796541f2f033131782aad16dab2b3d413f48
Philip Peiffer <philip.peiffer@gmail.com>
2022-12-09 12:02:33 -0700

    base mine_neighbor functionality

--- mine_neighbors.py
+++ mine_neighbors.py
@@ -1,1 +1,26 @@
 # This module contains an abstraction for neighbors to mines
+from space import Space
+
+class MineNeighbor(Space):
+    
+    def __init__(self, row, col, board) -> None:
+        super().__init__(row, col, board)
+        self.m_touching = 0
+
+
+    def put_on_board(self):
+        super().put_on_board()
+        self.bind("<Button-1>", self.__select)
+        self.config(text=f"{self.m_touching}")
+        self.grid(row=self.row, column=self.col)
+
+
+    def __select(self, event):
+        super().__select()
+
+
+    def add_mine(self):
+        """
+        Increments a mine neighbor's m_touching value.
+        """
+        self.m_touching += 1
