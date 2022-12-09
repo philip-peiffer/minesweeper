4a3cd4dd58c498263399ac96d6b3e83e59cf0c03
Philip Peiffer <philip.peiffer@gmail.com>
2022-12-09 11:27:14 -0700

    implemented small mine functionality

--- mine.py
+++ mine.py
@@ -1,8 +1,23 @@
 # This module contains an abstraction for the mines used in the game
 from space import Space
 
+class MineError(Exception):
+    pass
+
 class Mine(Space):
     bg_color = "black"
     
-    def __init__(self, row, col, master) -> None:
-        pass
+    def __init__(self, row, col, board) -> None:
+        super().__init__(row, col, board)
+
+
+    def put_on_board(self):
+        super().put_on_board()
+        self.bind("<Button-1>", self.__select)
+        self.config(background="black")
+        self.grid(row=self.row, column=self.col)
+
+
+    def __select(self, event):
+        super().__select()
+        raise MineError
