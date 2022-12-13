f522d82c0786417c4cb73c4828fef727fee084ac
Philip Peiffer <philip.peiffer@gmail.com>
2022-12-13 11:49:52 -0700

    appending button 3 event

--- space.py
+++ space.py
@@ -39,7 +39,7 @@ class Space(Button):
         )
 
         # bind right mouse click actions to button
-        self.bind("<Button-3>", self.__toggle_suspect)
+        self.bind("<Button-3>", self.__toggle_suspect, "+")
         self.bind("<Button-1>", self.select, "+")
         self.grid(row=self.row, column=self.col)
 
